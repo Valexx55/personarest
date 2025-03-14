63edcc6dff17d51b89b4cf5b74fbc8daf69d93ec
Val <valexx55@gmail.com>
2025-03-14 18:23:54 +0100

    nueva config

--- src/main/java/edu/val/restprofe/controller/PersonaController.java
+++ src/main/java/edu/val/restprofe/controller/PersonaController.java
@@ -1,5 +1,6 @@
 package edu.val.restprofe.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,22 @@ import edu.val.restprofe.dto.Persona;
 @RequestMapping("/persona")
 public class PersonaController {
 	
+	
+	@Value("${mientorno}")
+	String mientorno;
+	
 	@GetMapping
 	public Persona obtenerPersona ()
 	{
 		return new Persona(1L, "Vale", 41);
+	}
+	
+	
+	@GetMapping("/entorno")
+	public String obtenerEntorno ()
+	{
+		return mientorno;
+		
 	}
 	
 	
